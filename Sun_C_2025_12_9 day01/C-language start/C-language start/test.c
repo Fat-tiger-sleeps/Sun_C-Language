30720c468d05cb2a87a447b8e4b61beb17e9038e
Yuhang_warehouse <494977436@qq.com>
2025-12-09 19:17:03 +0800

    首次提交

---
+++ Sun_C_2025_12_9 day01/C-language start/C-language start/test.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+//main()代表函数   其中，main函数是函数的入口
+//函数是从main函数开始后的第一行，进行执行操作
+//int 是声明函数返回类型
+//{}  内部是函数体
+//return 0  代表函数执行结束后返回0，“0”作为整数与int返回类型相互呼应
+
+int main()
+{
+	printf("hello world!\n");
+
+	return 0;
+}
